964f9dd008c1b21568490fdc2bd071b049884ec7
giangth2310 <giangth2310@gmail.com>
2019-12-13 12:48:55 +0700

    test buffer

--- out/production/Yamero/test/gist/Server.java
+++ out/production/Yamero/test/gist/Server.java
@@ -1,34 +1,19 @@
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.util.Date;
+import java.io.*;
+import java.net.*;
 
 public class Server {
-  public static void main(String... args) throws IOException {
-    int port = args.length < 1 ? 55555 : Integer.parseInt(args[0]);
-    ServerSocketChannel ss = ServerSocketChannel.open();
-    ss.socket().bind(new InetSocketAddress(port));
-    while (!ss.socket().isClosed()) {
-      SocketChannel s = ss.accept();
-      System.out.println(new Date() + ": Accepted a connection");
-      long start = System.nanoTime();
-      ByteBuffer bytes = ByteBuffer.allocateDirect(32*1024);
-      int len;
-      long total = 0;
-      // Thank you @EJP, for a more elegant single loop.
-      while ((len = s.read(bytes)) >= 0 || bytes.position() > 0) { 
-        bytes.flip(); 
-        s.write(bytes); 
-        bytes.compact(); 
-        total += len;
-      }
-      long time = System.nanoTime() - start;
-      System.out.printf(new Date() + ": Transfer rate was %.1f MB/s%n", total * 1e9 / 1024 / 1024 / time);
+  public static void main(String[] args) throws Exception {
+    ServerSocket server = new ServerSocket(6666);
+    Socket socket = server.accept();
+    BufferedOutputStream output = new BufferedOutputStream(socket.getOutputStream());
+
+    byte[] bytes = new byte[10 * 1024]; // 10K
+    for (int i = 0; i < bytes.length; i++) {
+      bytes[i] = 12;
     }
-    ss.close();
+
+    while (true) {
+      output.write(bytes);
+    }
   }
 }
